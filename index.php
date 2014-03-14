8199ad0debbfbccfb9ad0074136b489895dbe799
sahilng <sahilng1997@gmail.com>
2014-03-14 14:21:41 -0400

    added base files

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head><title>Schedules</title></head>
+
+<link rel="stylesheet" type="text" href="style.css">
+
+<body>
+
+<div id="header">
+	
+	<div id="logo"><a href="index.php">Schedules</a></div>
+	
+</div>
+
+
+<div id="login">
+<form action="base.php" method="post">
+<br>
+<div id="uid_container">
+<input id="uid" type="text" name="uid" placeholder="HM Unique ID">
+</div>
+<br>
+<div id="pw_container">
+<input id="pw" type="password" name="pw" placeholder="Secret Password">
+</div>
+<input id="login_submit" type=submit>
+</form>
+
+</div>
+
+
+</body>
+</html>
