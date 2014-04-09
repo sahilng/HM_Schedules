099db71958550975afbbde56e91d4932e44e5a46
sahilng <sahilng1997@gmail.com>
2014-04-09 06:54:58 -0400

    added Google Analytics

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head><title>Schedules</title></head>
+<head><title>Schedules</title>
 
 <link rel="stylesheet" type="text" href="style.css">
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48300891-2', 'gevva.com');
+  ga('send', 'pageview');
+
+</script>
+
+</head>
 <body>
 
 <div id="header">
